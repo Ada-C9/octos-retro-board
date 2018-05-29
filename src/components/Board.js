ba8d20da4a523bc2e1a84c18e1a29bcf19097e1e
Dan Roberts <dan@adadevelopersacademy.org>
2018-05-29 11:50:23 -0700

    Add PropTypes for Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Sector from './Sector';
 
 import './Board.css';
 
 class Board extends React.Component {
+  static propTypes = {
+    retroData: PropTypes.arrayOf(
+      PropTypes.shape({
+        sector: PropTypes.string,
+        text: PropTypes.string,
+        finished: PropTypes.bool,
+      })
+    ).isRequired,
+    sectorNames: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }
+
   getMessages(sectorName) {
     return this.props.retroData.filter((message) => {
       return message.sector === sectorName;
